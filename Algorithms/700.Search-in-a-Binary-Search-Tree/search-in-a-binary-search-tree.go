75ca2006bfad88512b561f0a782c7f1642d50785
CLaterFall <laterfall.lian@gmail.com>
2018-12-19 17:30:04 +0800

    add python & go solution

---
+++ Algorithms/700.Search-in-a-Binary-Search-Tree/search-in-a-binary-search-tree.go
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func searchBST(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	switch {
+	case root.Val > val:
+		return searchBST(root.Left, val)
+	case root.Val < val:
+		return searchBST(root.Right, val)
+	default:
+		return root
+	}
+}
