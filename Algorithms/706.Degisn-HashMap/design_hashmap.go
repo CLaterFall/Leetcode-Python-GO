127eb06415654595ea2ed02d13d04553182aa121
CLaterFall <laterfall.lian@gmail.com>
2019-01-07 17:13:38 +0800

    add 706 python&go solution

---
+++ Algorithms/706.Degisn-HashMap/design_hashmap.go
@@ -0,0 +1,36 @@
+type MyHashMap struct {
+	table []int
+}
+
+/** Initialize your data structure here. */
+func Constructor() MyHashMap {
+	newHashMap := MyHashMap{table: make([]int, 1000001)}
+	var i int
+	for i = 0; i < 1000001; i++ {
+		newHashMap.table[i] = -1
+	}
+	return newHashMap
+}
+
+/** value will always be non-negative. */
+func (this *MyHashMap) Put(key int, value int) {
+	this.table[key] = value
+}
+
+/** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+func (this *MyHashMap) Get(key int) int {
+	return this.table[key]
+}
+
+/** Removes the mapping of the specified value key if this map contains a mapping for the key */
+func (this *MyHashMap) Remove(key int) {
+	this.table[key] = -1
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Put(key,value);
+ * param_2 := obj.Get(key);
+ * obj.Remove(key);
+ */
