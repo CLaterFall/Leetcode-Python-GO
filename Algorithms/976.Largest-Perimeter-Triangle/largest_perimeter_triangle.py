468077dd8f22e8b41c42407684b3bb2c2c441596
CLaterFall <laterfall.lian@gmail.com>
2019-04-15 14:57:36 +0800

    add 976 python&go solution

---
+++ Algorithms/976.Largest-Perimeter-Triangle/largest_perimeter_triangle.py
@@ -0,0 +1,10 @@
+class Solution:
+    def largestPerimeter(self, A: List[int]) -> int:
+        A.sort(reverse=True)
+        if len(A) < 3:
+            return 0
+        for i in range(len(A) - 2):
+            if A[i] < A[i + 1] + A[i + 2]:
+                return A[i] + A[i+1] + A[i+2]
+        return 0
+        
