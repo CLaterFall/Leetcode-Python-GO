b0cff54c255de3187409104bb477aa4b12a28e24
CLaterFall <CLaterFall>
2019-06-16 20:50:54 +0800

    add 520 python&go solution

---
+++ Algorithms/520.Detect-Capital/detect_capital.go
@@ -0,0 +1,3 @@
+func detectCapitalUse(word string) bool {
+    return strings.ToUpper(word) == word || strings.ToLower(word[1:]) == word[1:]
+}
---
+++ Algorithms/520.Detect-Capital/detect_capital.py
@@ -0,0 +1,3 @@
+class Solution:
+    def detectCapitalUse(self, word: str) -> bool:
+        return word.islower() or word.istitle() or word.isupper() or word == '' 
